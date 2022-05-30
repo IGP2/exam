ca5b426554d13cad74f8f213aafa49f4113c3817
IGP2 <igorprzybysz@gmail.com>
2022-05-30 21:43:09 +0200

    release0.1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo'<!DOCTYPE html>';
+echo'<html lang="en">';
+echo'<head>';
+    echo'<meta charset="UTF-8">';
+    echo'<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+    echo'<meta name="viewport" content="width=, initial-scale=1.0">';
+    echo'<title>Igor Przybysz</title>';
+    echo'</head>';
+    echo'<body>';
+    echo date('H:i:s d/m/Y');
+    echo'</body>';
+    echo'</html>';
+?>
